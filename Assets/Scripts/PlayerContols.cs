8ac71ac4a29c773ed8f592529ef14504c806f7fe
alejandro-deltell <alejandro.deltell@gmail.com>
2022-01-31 20:54:53 +0100

    - Terrain - X & Y movements. - Updated Player script.

---
+++ Assets/Scripts/PlayerContols.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContols : MonoBehaviour
+{
+
+    [SerializeField] float controlSpeed = 10f;
+    [SerializeField] float xRange = 8f;
+    [SerializeField] float yRange = 5f;
+
+
+    void Update()
+    {
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3 (clampedXPos, clampedYPos, transform.localPosition.z);
+
+    }
+}
